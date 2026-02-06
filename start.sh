02e327f7c37cb504cc17031ae54ee121ebbf2a48
uscopyrightofficer-spec <uscopyrightofficer@gmail.com>
2026-02-06 23:24:19 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-curl -L https://github.com/projectdiscovery/httpx/releases/download/v1.6.5/httpx_1.6.5_linux_amd64.zip -o httpx.zip
+echo "Installing httpx..."
+
+curl -L https://github.com/projectdiscovery/httpx/releases/latest/download/httpx_linux_amd64.zip -o httpx.zip
 unzip httpx.zip
 chmod +x httpx
+mv httpx /usr/local/bin/httpx
 
+echo "Starting bot..."
 python bot.py
